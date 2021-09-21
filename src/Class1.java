5338ad7b8306f7d504613cb242180ee40f7242bc
EmsCode82 <emsgithub@gmail.com>
2021-09-20 21:35:30 -0500

    Exercise Methods

---
+++ src/Class1.java
@@ -0,0 +1,13 @@
+import java.math.BigDecimal;
+
+public class Class1 {
+	public static void main(String[] args) {
+		double x = 1.05;
+		double y = 2.55;
+		BigDecimal d1 = new BigDecimal("1.05");
+		BigDecimal d2 = new BigDecimal("2.55");
+		System.out.println(d1.add(d2));
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
